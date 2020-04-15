25e2257492bce86bc72399c1cc9b43edc7674d20
Ryan Cruz <rncb0963@gmail.com>
2020-04-15 01:04:50 -0500

    Added error P0000

--- src/Error.cpp
+++ src/Error.cpp
@@ -1,32 +1,25 @@
-inline class Error {
-    
-    public:
+#pragma once
 
-    void updateLine(int lineNumber ){ this->currentLine = lineNumber; }
-    void updateChar(int charNumber ){ this->currentChar = charNumber; }
-
-    void getError(ErrorType error){ return errors[error] }
-
-    private:
+inline namespace Error {
 
     int currentLine;
     int currentChar;
 
-    const std::string errors[] = {
-        
-
-    }
+    void updateLine(int lineNumber ){ currentLine = lineNumber; }
+    void updateChar(int charNumber ){ currentChar = charNumber; }
 
     const std::string
     
     /* Parser Errors */
+    //|pmicro your_file.micro -o output.asm
 
+    P0000 = "|kP0000 |rERROR: |wNo file or flags specified. Try using |pmicro --help",
     P0001 = "Expected comma in line " + std::to_string(currentLine),
     P0002 = "Something else",
     
 
     /* Compiler Errors */
 
-    C0001 = "Something"
+    C0001 = "Something";
 
-} 
+};
