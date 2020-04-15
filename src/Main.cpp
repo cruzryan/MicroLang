c050e66ca9ce81db912e8b8429a0f0dab11ae82d
Ryan Cruz <rncb0963@gmail.com>
2020-04-14 19:37:26 -0500

    Testing the logger now works

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
-#include "Parser.cpp"
-#include "Error.cpp"
+#include <fstream>
+#include "PlatformCheck.cpp"
+#include "Logger.cpp"
+//#include "Parser.cpp"
+//#include "Error.cpp"
 
 using namespace std;
 
+const bool DEBUG_MODE = true;
+
 int main(int argc, char *argv[]){
 	
+	if (DEBUG_MODE){
+
+		Logger::Log("Running in |rDEBUG_MODE");
+		Logger::Log("|kP0001 |rERROR: |wexpected |r; |win Main.micro|y line 32");
+
+
+	}else{
+
+	
+	}
+
 	cout << "main fn" << endl;
 }
