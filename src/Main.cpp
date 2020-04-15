758d3d90df71e3acf898d525c9cb9a666acbffe5
Ryan Cruz <rncb0963@gmail.com>
2020-04-15 01:05:18 -0500

    Cleaned Up

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,24 +2,19 @@
 #include <fstream>
 #include "PlatformCheck.cpp"
 #include "Logger.cpp"
+#include "Error.cpp"
+#include "Console.cpp"
+
 //#include "Parser.cpp"
-//#include "Error.cpp"
 
 using namespace std;
 
 const bool DEBUG_MODE = true;
 
 int main(int argc, char *argv[]){
-	
-	if (DEBUG_MODE){
 
-		Logger::Log("Running in |rDEBUG_MODE");
-		Logger::Log("|kP0001 |rERROR: |wexpected |r; |win Main.micro|y line 32");
+	Logger::Log("|bMicroLang |r(Beta)\n");
 
-
-	}else{
-
-	
-	}
+	Console::CheckParameters(argc,argv);
 
 }
