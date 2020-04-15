660055267ae57aeaed8d4e26aa4004a21c8e672c
Ryan Cruz <rncb0963@gmail.com>
2020-04-14 19:36:34 -0500

    Rewrite of the Logger

---
+++ src/Logger.cpp
@@ -0,0 +1,90 @@
+inline namespace Logger {
+
+
+    enum WindowsColor {
+
+        WHITE = 0x0F,
+        RED = 0x0C,
+        GREEN = 0x0A,
+        BLUE = 0x09,
+        YELLOW = 0x0E,
+        PURPLE = 0x0D,
+        GREY = 0x08
+
+    };
+
+    const char& separator = '|';
+
+    /*
+        Color codes: 
+
+        &w = white
+        &r = red
+        &g = green
+        &b = blue
+        &y = yellow
+        &p = purple
+        &k = grey
+
+    */
+
+    void changeColor(const char* c){
+
+        
+        if(PLATFORM_NAME == "windows"){
+            HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            switch(c[0]){
+                case 'w':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::WHITE);
+                break;
+                case 'r':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::RED);
+                break;
+                case 'g':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::GREEN);
+                break;
+                case 'b':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::BLUE);
+                break;
+                case 'y':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::YELLOW);
+                break;
+                case 'p':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::PURPLE);
+                break;
+                case 'k':
+                    SetConsoleTextAttribute(hConsole, WindowsColor::GREY);
+                break;
+            }
+
+
+            
+            return;
+        }
+
+       // std::cout << "Changing color as : " << c[0] << std::endl;
+       
+    }
+
+    void Log(std::string message){
+
+        for(int i = 0; i < message.size(); i++){
+            if(message.at(i) == separator){
+
+                changeColor(&message[i+1]);
+                i = i + 1;
+
+            }else{
+                std::cout << message.at(i);
+            }
+        }       
+
+        changeColor("w");
+        std::cout << "" << std::endl;
+
+    
+    }
+
+
+}
