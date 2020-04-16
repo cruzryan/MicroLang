a4de86846b16eb5a0c7e167d3ef3e4732d3af669
Ryan Cruz <rncb0963@gmail.com>
2020-04-16 00:11:49 -0500

    Some ideas about testing

--- src/Test.cpp
+++ src/Test.cpp
@@ -1,5 +1,22 @@
 namespace Test {
 
-    
+    std::string root = "../tests/";
+
+    std::string dirs[] = {
+        "variables/assign_variables"
+
+    };
+
+    void All(){
+        Logger::Log("|gTesting all features |k(this might take some time)");  
+        ///TO-DO
+
+    }
+
+    void Path(std::string path){
+        Logger::Log("|gTesting |w" + path);        
+        ///TO-DO
+    }
+
 
 }
