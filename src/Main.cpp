d659961340ba63e642553be140667bbd874d689b
Ryan Cruz <rncb0963@gmail.com>
2020-04-16 00:10:02 -0500

    Setup dev mode

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <fstream>
+
+#define DEVMODE false
+
 #include "PlatformCheck.cpp"
 #include "Logger.cpp"
 #include "Error.cpp"
+#include "Test.cpp"
 #include "Console.cpp"
 
 //#include "Parser.cpp"
 
 using namespace std;
 
-const bool DEBUG_MODE = true;
-
 int main(int argc, char *argv[]){
 
 	Logger::Log("|bMicroLang |r(Beta)\n");
-
 	Console::CheckParameters(argc,argv);
 
-}
+} 
