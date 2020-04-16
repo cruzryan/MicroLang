530da0ec1b60226412ff64d921238a63d7a5563e
Ryan Cruz <rncb0963@gmail.com>
2020-04-16 00:10:55 -0500

    Started implementing console

--- src/Console.cpp
+++ src/Console.cpp
@@ -1,16 +1,46 @@
 namespace Console {
 
-    //Logger::Log("Running in |rDEBUG_MODE");
-
     void CheckParameters(int n, char *args[]){
             
-	    //If no arguments are passed to the compiler 
-	    if(n == 1){
 
-		//Show error and close the program
-		Logger::Panic(Error::P0000);
+	    //If no arguments are passed to the compiler panic
+	    if(n == 1) Logger::Panic(Error::A0000);
 
-	    }
+
+        /*
+        *           TESTS
+        */
+
+
+        //If "test" is passed as a parameter
+        if(args[1] == std::string("test")){
+
+            //if "test" is passed with no args then test all 
+            if(n == 2) Test::All();
+            
+            //if a file path is passed as a parameter
+            if(n == 3) Test::Path(args[2]);
+
+
+        }
+
+        /*
+        *           BUILDS
+        */
+
+        if(args[1] == std::string("build")){
+            ///TO-DO
+        }
+
+
+        /*
+        *           DEVELOPER DEBUG
+        */
+
+        if(args[1] == std::string("dev")){
+            #define DEVMODE true
+            Logger::Log("|gDev Mode started");
+        }
 
     }
 }
