7067f221d1b2e04e36b299eff6bf0e52515a7ee0
Ryan Cruz <rncb0963@gmail.com>
2020-04-17 21:18:26 -0500

    Create AST.cpp

---
+++ src/AST.cpp
@@ -0,0 +1,17 @@
+class AST {
+    
+    private:
+
+        struct Node {
+            
+            TokenType tt;
+            std::vector<*Node> nodes;
+               
+
+        }
+
+    public:
+
+
+
+};
